a483541d20fef8d1967b926b244d29f6bce55ae3
a1isahib <101056016+alisahibmammadov@users.noreply.github.com>
2023-07-26 02:54:46 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const app = express();
 app.use(cors());
-  
+   
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
